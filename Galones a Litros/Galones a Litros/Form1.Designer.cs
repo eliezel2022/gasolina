3fded05238e2fc661003f64c451bfb7a77bb8e89
eliezel2022 <98374618+eliezel2022@users.noreply.github.com>
2022-08-19 13:48:57 -0400

    Add files via upload

---
+++ Galones a Litros/Galones a Litros/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace Galones_a_Litros
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.dtgv = new System.Windows.Forms.DataGridView();
+            this.cantidad2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.cantidad1 = new System.Windows.Forms.TextBox();
+            this.catidada = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.gasolina = new System.Windows.Forms.TextBox();
+            this.ide = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.litros = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.galones = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.precio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Location = new System.Drawing.Point(170, 392);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 0;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(271, 392);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // dtgv
+            // 
+            this.dtgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ide,
+            this.litros,
+            this.galones,
+            this.precio});
+            this.dtgv.Location = new System.Drawing.Point(12, 222);
+            this.dtgv.Name = "dtgv";
+            this.dtgv.Size = new System.Drawing.Size(393, 150);
+            this.dtgv.TabIndex = 2;
+            // 
+            // cantidad2
+            // 
+            this.cantidad2.Location = new System.Drawing.Point(276, 52);
+            this.cantidad2.Multiline = true;
+            this.cantidad2.Name = "cantidad2";
+            this.cantidad2.Size = new System.Drawing.Size(53, 20);
+            this.cantidad2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Precio de la Gasolina: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(240, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Introduzca la cantidad de Litros en pista:";
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCerrar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnCerrar.Location = new System.Drawing.Point(68, 392);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCerrar.TabIndex = 6;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // cantidad1
+            // 
+            this.cantidad1.Location = new System.Drawing.Point(275, 18);
+            this.cantidad1.Multiline = true;
+            this.cantidad1.Name = "cantidad1";
+            this.cantidad1.Size = new System.Drawing.Size(53, 20);
+            this.cantidad1.TabIndex = 7;
+            // 
+            // catidada
+            // 
+            this.catidada.Location = new System.Drawing.Point(154, 92);
+            this.catidada.Multiline = true;
+            this.catidada.Name = "catidada";
+            this.catidada.Size = new System.Drawing.Size(53, 20);
+            this.catidada.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(9, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(251, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Introduzca la cantidad de Litros en ciudad:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Cantidad de pasos :";
+            // 
+            // gasolina
+            // 
+            this.gasolina.Location = new System.Drawing.Point(154, 126);
+            this.gasolina.Multiline = true;
+            this.gasolina.Name = "gasolina";
+            this.gasolina.Size = new System.Drawing.Size(53, 20);
+            this.gasolina.TabIndex = 8;
+            // 
+            // ide
+            // 
+            this.ide.HeaderText = "ID";
+            this.ide.Name = "ide";
+            this.ide.Width = 80;
+            // 
+            // litros
+            // 
+            this.litros.HeaderText = "Litros/km";
+            this.litros.Name = "litros";
+            this.litros.Width = 80;
+            // 
+            // galones
+            // 
+            this.galones.HeaderText = "Galones";
+            this.galones.Name = "galones";
+            this.galones.Width = 80;
+            // 
+            // precio
+            // 
+            this.precio.HeaderText = "Precio\\GL";
+            this.precio.Name = "precio";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 454);
+            this.Controls.Add(this.catidada);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.gasolina);
+            this.Controls.Add(this.cantidad1);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cantidad2);
+            this.Controls.Add(this.dtgv);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.btnLimpiar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.DataGridView dtgv;
+        private System.Windows.Forms.TextBox cantidad2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.TextBox cantidad1;
+        private System.Windows.Forms.TextBox catidada;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox gasolina;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ide;
+        private System.Windows.Forms.DataGridViewTextBoxColumn litros;
+        private System.Windows.Forms.DataGridViewTextBoxColumn galones;
+        private System.Windows.Forms.DataGridViewTextBoxColumn precio;
+    }
+}
+
---
+++ Galones a Litros/Galones a Litros/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Galones_a_Litros
+{
+    public partial class Form1 : Form
+    {
+        public double a = 0;
+        public double cash;
+        public double valor;
+        public double gal;
+        public int count = 0;
+        public int id = 1;
+        public double j;
+        public double paso;
+        public double save;
+        public double numero1;
+        public double numero2;
+        public double numero3;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double b = Convert.ToDouble(cantidad1.Text);
+            double a = Convert.ToDouble(cantidad2.Text);
+            double cash = Convert.ToDouble(cantidad1.Text);
+            double paso = Convert.ToDouble(catidada.Text);
+
+            if (b < a)
+            {
+                string message = "el vaor de ciudad es regularmante mas alto";
+                string caption = "Error";
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                DialogResult result;
+
+                result = MessageBox.Show(message, caption, buttons);
+
+            }
+            while (a <= b)
+            {
+                gal = b * 0.264172;
+                j = gal / 100;
+                valor = cash * j;
+                int n = dtgv.Rows.Add();
+                valor = Math.Round(valor, 2);
+                gal = Math.Round(gal, 2);
+                dtgv.Rows[n].Cells[0].Value = id;
+                dtgv.Rows[n].Cells[1].Value = b + "/100Km";
+                dtgv.Rows[n].Cells[2].Value = gal;
+                dtgv.Rows[n].Cells[3].Value = "DOP$ " + valor;
+                b = b - paso;
+                id++;
+
+            }
+            save = id;            
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            cantidad1.Text = "";
+            dtgv.Rows.Clear();
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
